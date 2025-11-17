3833ade4544b52f2d804df592176d04919365b4b
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-11-17 15:59:01 -0300

    Finalizado as seções de restaurante e serviços e começando a seção de mão na massa

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Hero from "./components/landing_page/hero"
-
+import SectionMaoNaMassa from "./components/landing_page/sectionMaoNaMassa"
+import SectionRestaurant from "./components/landing_page/sectionRestaurant"
+import SectionServices from "./components/landing_page/sectionServices"
 
 function App() {
   return (
-    <Hero />
+    <main className="bg-gray-50">
+      <Hero />
+      <SectionServices />
+      <SectionRestaurant />
+      <SectionMaoNaMassa />
+    </main>
   )
 }
 
---
+++ src/components/landing_page/sectionRestaurant.tsx
@@ -0,0 +1,45 @@
+import { carouselImages } from "@/assets/assets";
+import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+
+export default function SectionRestaurant() {
+    return (
+        <section className="w-full flex flex-col items-center justify-center pb-16 bg-gray-50">
+            <div className="flex flex-row justify-between items-center max-w-7xl w-full">
+                <h2 className="text-3xl font-bold text-gray-800">Os melhores restaurantes da cidade</h2>
+
+                <Button variant="ghost">
+                    Ver todos
+                </Button>
+            </div>
+
+            <div className="mt-16">
+                <Carousel className="w-full max-w-7xl">
+                    <CarouselContent>
+                        {carouselImages.map((image, index) => {
+                            return (
+                                <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                    <div className="p-2">
+                                        <Card>
+                                            <CardContent className="flex items-center justify-center">
+                                                <img 
+                                                    src={image.src} 
+                                                    alt={image.alt} 
+                                                    className="h-64 w-full object-cover rounded-lg shadow-md transition-transform duration-600 transform hover:scale-105"
+                                                />
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            )
+                        })}
+                    </CarouselContent>
+                    
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+        </section>
+    )
+}
